42093b911cd149c716c5ebc70d1928f7c7d89ab6
lluismarcello333 <98034550+lluismarcello333@users.noreply.github.com>
2024-01-12 11:21:33 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import Home from "./pages/Home";
@@ -11,7 +11,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
@@ -19,6 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="search" element={<Search />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
