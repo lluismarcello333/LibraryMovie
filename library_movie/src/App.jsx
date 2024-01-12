9759fe0e24e05059d314ce4352ed3f84a3a19fe6
Luiz Marcelo <lluismarcello333@gmail.com>
2024-01-12 09:30:50 -0300

    Criação de uma Biblioteca de Filmes

---
+++ library_movie/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ library_movie/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./App.jsx";
+import Home from "./Pages/Home.jsx";
+import Movie from "./Pages/Movie.jsx";
+import Search from "./Pages/Search.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
